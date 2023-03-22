482fce76aab68bcba646f58d05f83b2806e45849
zaideemendoza <zaideemendoza75@gmail.com>
2023-03-22 08:20:51 +0800

    Intern Interface

--- home.php
+++ home.php
@@ -6,10 +6,73 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>Intern Interface</title>
+    
 </head>
-<body>
- 
+<body onload="initClock()">
+
+<!-- Digital Clock Start -->
+<div class="datetime">
+    <div class="date">
+        <span id = "dayname"> Day </span>,
+        <span id = "month"> Month </span>
+        <span id = "daynum"> 00 </span>,
+        <span id = "year"> Year </span>
+    </div>
+    <div class="time">
+        <span id = "hour"> 00 </span>:
+        <span id = "minutes"> 00</span>:
+        <span id = "seconds"> 00 </span>
+        <span id = "period"> AM </span> 
+    </div>
+</div>
+
+<script type="text/javascript">
+    function updateClock() {
+        var now = new Date();
+        var dname =  now.getDay();
+            mo = now.getMonth(),
+            dnum= now.getDate(),
+            yr = now.getFullYear();
+            hou = now.getHours();
+            min = now.getMinutes();
+            sec = now.getSeconds();
+            pe = "AM";
+
+            if(hou == 0){
+                hou = 12;
+            }
+
+            if(hou > 12){
+                hou = hou - 12;
+                pe = "PM";
+
+            }
+            Number.prototype.pad = function(digits){
+                for (var n= this.toString(); n.length < digits; n= 0 + n);
+
+            }
+
+            var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov","Dec"]
+            var week =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday","Friday", "Saturday"]
+            var ids = ["dayname", "month","daynum", "year", "hour", "minutes", "seconds", "period"]
+            var values = [week[dname], months[mo], dnum, yr, hou, min, sec, pe];
+            for (var i = 0; i < ids.length; i++)
+            document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+     }
+
+     function initClock() {
+     updateClock();
+     window.setInterval("updateClock()",1);
+
+
+     }
+
+
+
+     </script>
+
+
 
 
 
