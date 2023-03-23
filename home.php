11a550179e795b53427e5a29254e89863eaa7953
Raffy Veloria <veloria.raffy03@gmail.com>
2023-03-23 10:55:59 +0800

    css

--- home.php
+++ home.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  
+  
     <title>Intern Interface</title>
     
 </head>
@@ -37,7 +39,7 @@
         <span id = "hour"> 00 </span>:
         <span id = "minutes"> 00</span>:
         <span id = "seconds"> 00 </span>
-        <span id = "period"> AM </span> 
+        <span id = "period"><h4>AM</h4>  </span> 
     </div>
 </div>
 
@@ -57,7 +59,7 @@
 </table>
 
 
-<!--2nd Table-->
+
 
 <section class="tableuser table-bordered">
 <table class="table">
@@ -73,7 +75,8 @@
     </tr>
 </thead>
 
-<!--Sample Output for the database-->
+
+
 <tbody>
     <td>1</td>
     <td>March 22, 2023</td>
@@ -87,6 +90,17 @@
 </section>
 
 
+
+
+
+
+
+
+<script>$(document).ready(function () {
+    $('#example').DataTable();
+});</script>
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 
 
 
--- login_intern.php
+++ login_intern.php
@@ -1,40 +1,4 @@
-<?php
-session_start(); // Call session_start() before any output is sent
 
-// Connect to the database
-$serverName = "LAPTOP-GBO9I3B3\SQL";
-$connectionOptions = [
-    "Database" => "DLSUD",
-    "Uid" => "",
-    "PWD" => ""
-];
-
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-// Check the connection
-if (!$conn) {
-    die("Connection failed: " . sqlsrv_errors());
-}
-
-$loginerr = ''; // Initialize login error message
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $form_id_log = isset($_POST["username"]) ? $_POST["username"] : '';
-    $user_pass_log = isset($_POST["userpassword"]) ? $_POST["userpassword"] : '';
-    // Select the FORM_ID and USER_PASSWORD from the users table
-    $sql = "SELECT USERNAME, USER_PASSWORD FROM FEDCENTER_INTERN_LOGIN WHERE USERNAME=? AND USER_PASSWORD=?";
-    $params = array($form_id_log, $user_pass_log);
-    $result = sqlsrv_query($conn, $sql, $params);
-    // Fetch the data from the database
- 
-    if (sqlsrv_has_rows($result) ) {
-        echo "<script>alert('WELCOME INTERN!');</script>";
-        exit();
-    } else {
-        $loginerr ="Invalid Username/Password.";
-    }
-}
-?>
 
 <!-- Samboy -->
 
@@ -53,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="login-wrapper">
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="form">
       <img src="img/FedCenter_Wolf-removebg-preview.png" alt="">
-      <h2>Admin Login</h2>
+      <h2>Intern Login</h2>
       <div class="input-group">
         <input type="text" name="username" id="username" required>
         <label for="username">User email</label>
@@ -66,22 +30,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <i id="hide2" class="fa-solid fa-eye-slash"></i></span>
       </div>
       <input type="submit" value="Login" class="submit-btn">
-      <a href="#forgot-pw" class="forgot-pw">Forgot Password?</a>
+      <a href="#forgot-pw" class="forgot-pw"><h4 class="primary-variant">Forgot Password?</h4></a>
     </form>
-                                    
+      <center>                              
     <div id="forgot-pw">
       <form action="" class="form">
         <a href="#" class="close">&times;</a>
         <h2>Reset Password</h2>
         <div class="input-group">
           <input type="email" name="email" id="email" required>
-          <label for="email">Email</label>
+          <label for="email"><h3 class="white">Email</h3></label>
         </div>
         <input type="submit" value="Submit" class="submit-btn">
       </form>
     </div>
   </div>
-  
+</center>
 <!--Password Hide-->
 <script>
     function myFunction(){
