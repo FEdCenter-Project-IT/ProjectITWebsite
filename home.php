39eb1fd05e380accc533bade759e5e9d85680554
Raffy Veloria <veloria.raffy03@gmail.com>
2023-03-28 16:20:12 +0800

    DropDown Menu Profile

--- home.php
+++ home.php
@@ -1,82 +1,336 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Data Tables</title>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-
-    <script
-  src="http://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
-  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Dynamic Pagination</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<div class="container">
-    <h2>Simple Pagination Example using Datatables Js Library</h2>
-    <table class="table table-fluid" id="myTable">
-    <thead>
-    <tr>
-      <th>Name</th>
-      <th>Email</th>
-      <th>Password</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <td>Daniel Danny</td>
-      <td>danny.daniel@gmail.com</td>
-      <td>Pass1234</td>
-    </tr>
-    <tr>
-      <td>Samuel</td>
-      <td>samuel@gmail.com</td>
-      <td>Pass1234</td>
-    </tr>
-    <tr>
-      <td>Jack</td>
-      <td>jack@gmail.com</td>
-      <td>Pass1234</td>
-    </tr>
-    <tr>
-      <td>Eureka</td>
-      <td>eureka@gmail.com</td>
-      <td>Pass1234</td>
-    </tr>
-    <tr>
-      <td>Pinky</td>
-      <td>pinky@gmail.com</td>
-      <td>Pass1234</td>
-    </tr>
-    <tr>
-      <td>Mishti</td>
-      <td>mishti@gmail.com</td>
-      <td>Pass1234</td>
-    </tr>
-    <tr><td>Puneet</td>
-    <td>puneet@gmail.com</td>
-    <td>Pass1234</td>
-  </tr>
-    <tr><td>Nick</td>
-    <td>nick@gmail.com</td>
-    <td>Pass1234</td>
-  </tr>
-    
-    </tbody>
-    </table>
-</div>
+
+<nav>
+  <img src="img/FedCenter_Logo-removebg-preview.png" class="logo" >
+  <ul>
+    <li> <a href="">Home</a> </li>
+    <li> <a href="">About</a> </li>
+    <li> <a href="">Contact</a> </li>
+    <li> <a href="">Blogs</a> </li>
+  </ul>
+  <img src="img/zoomDP.jpg" class="user_pic" onclick="toggleMenu()">
+
+  <div class="sub-menu-wrap" id="subMenu">
+    <div class="sub-menu">
+      <div class="user-info">
+        <img src="img/zoomDP.jpg">
+        <h2>Raffy L. Veloria</h2>
+      </div>
+      <hr>
+
+      <a href="" class="sub-menu-link">
+      <i class="fa fa-user"></i>
+        <p>Edit Profile</p>
+        <span>></span>
+      </a>
+      <a href="" class="sub-menu-link">
+      <i class="fa fa-gear"></i>
+        <p>Setting & Privacy</p>
+        <span>></span>
+      </a>
+      <a href="" class="sub-menu-link">
+      <i class="fa-solid fa-circle-question"></i>
+        <p>Help & Support</p>
+        <span>></span>
+      </a>
+      <a href="" class="sub-menu-link">
+      <i class="fa-solid fa-right-from-bracket"></i>
+        <p>Logout</p>
+        <span>></span>
+      </a>
+    </div>
+  </div>
+
+</nav>
 
 <script>
-    $(document).ready( function () {
-    $('#myTable').DataTable();
-} );
-    </script>
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+  subMenu.classList.toggle("open-menu");
+}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--- table 1
+
+<table class="userinfo">
+<tr>
+<th class="info">NAME:</th>
+<th class="info">FID:</th>
+<th class="info">PROJECTS:</th>
+<th class="info">TOTAL HOURS:</th>
+<th class="info">REQUIRED HOURS:</th>
+<th class="info"> REMAINING TIME:</th>
+</tr>
+</table>
+/*Table 1*/
+.userinfo {
+  font-family: Arial, Helvetica, sans-serif;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  width: 40%;
+  
+}
+.info {
+
+  text-align: justify;
+  margin-left: 15px;
+  display: block;
+  font-size: 20px;  
+  letter-spacing: 1px;
+  padding: 2px;
+  justify-content: right;
+}
+-->
+<!-- Table 2-->
+
+    
+
+
+<!---
+<table class="mytable2" id="mytable">
+
+
+ <tr class="infoget">
+
+ <th class="infotable2">NO</th>
+
+ <th class="infotable2">DATE</th>
+
+ <th class="infotable2">IN</th>
+
+ <th class="infotable2">OUT</th>
+
+ <th class="infotable2">HOURS </th>
+
+ <th class="infotable2">MINUTES</th>
+
+ <th class="infotable2">SPECIAL EVENT</th>
+
+ </tr>
+
+
+  <tr class="infoget">
+
+ <td>1</td>
+
+ <td>March 27, 2023</td>
+
+ <td>8:00AM</td>
+
+ <td>5:00PM</td>
+
+ <td>5</td>
+
+ <td>0</td>
+
+ <td>Typing EVent</td>
+
+ </tr>
+
+ <tr class="infoget">
+
+ <td>2</td>
+
+ <td>March 28, 2023</td>
+
+ <td>8:00AM</td>
+
+ <td>5:00PM</td>
+
+ <td>5</td>
+
+ <td>0</td>
+
+ <td>WWE</td>
+
+ </tr>
+
+ <tr class="infoget">
+
+ <td>3</td>
+
+ <td>March 29, 2023</td>
+
+ <td>8:00AM</td>
+
+ <td>5:00PM</td>
+
+ <td>5</td>
+
+ <td>0</td>
+
+ <td>TeamWork</td>
+
+ </tr>
+
+ <tr class="infoget">
+
+ <td>4</td>
+
+ <td>March 27, 2023</td>
+
+ <td>8:00AM</td>
+
+ <td>5:00PM</td>
+
+ <td>5</td>
+
+ <td>0</td>
+
+ <td>FIS</td>
+
+ </tr>
+ <tr class="infoget">
+
+<td>5</td>
+
+<td>March 30, 2023</td>
+
+<td>8:00AM</td>
+
+<td>5:00PM</td>
+
+<td>5</td>
+
+<td>0</td>
+
+<td>Sleep</td>
+
+</tr>
+<tr class="infoget">
+
+<td>6</td>
+
+<td>March 31, 2023</td>
+
+<td>8:00AM</td>
+
+<td>5:00PM</td>
+
+<td>5</td>
+
+<td>0</td>
+
+<td>Eat</td>
+
+</tr>
+<tr class="infoget">
+
+<td>7</td>
+
+<td>March 27, 2023</td>
+
+<td>8:00AM</td>
+
+<td>5:00PM</td>
+
+<td>5</td>
+
+<td>0</td>
+
+<td>Getting info</td>
+
+</tr>
+<tr class="infoget">
+
+<td>8</td>
+
+<td>March 27, 2023</td>
+
+<td>8:00AM</td>
+
+<td>5:00PM</td>
+
+<td>5</td>
+
+<td>0</td>
+
+<td>Onboard</td>
+
+</tr>
+
+ </table>
+
+ /*Table 2 css
+.mytable2{
+  position: absolute;
+  left: 17%;
+  top: 75%;
+  width: 70%;
+  border-spacing: 0;
+  border-collapse: collapse;
+  box-shadow: 0 2px 15px rgba(64,64,64,.7);
+  border-radius: 12px 12px 0 0;
+  overflow: hidden;
+ }
+
+ td , .infotable2{
+  padding: 15px 20px;
+  text-align: center;
+  
+ 
+ }
+ .infotable2{
+  background: var(--color-primary);
+  color: #fafafa;
+  font-family: 'Open Sans',Sans-serif;
+  font-weight: 200;
+  text-transform: uppercase;
+  font-weight: 600;
+ 
+ }
+ .infoget{
+  width: 100%;
+  background-color: #fafafa;
+  font-family: 'Montserrat', sans-serif;
+  transition: all .1 ease-in;
+  cursor: pointer;
+ }
+ .infoget:nth-child(even){
+  background-color: #eeeeee;
+ }
+.infoget:hover {
+  background: transparent;
+  transform: scale(1.02);
+  box-shadow: 0 2px 15px rgba(64,64,64,.7);
+}
+table.dataTable thead .sorting {
+  background-color: orange;
+}
+*/
+/*Table 2 */
+-->
+
+
 </body>
 </html>
--- homepage.php
+++ homepage.php
@@ -9,7 +9,46 @@
     <title>FIS</title>
 </head>
 <body onload="initClock()">
+<nav>
+  <img src="img/FedCenter_Logo-removebg-preview.png" class="logo" >
+  <ul>
+    <li> <a href="">Task</a> </li>
+  </ul>
+  <img src="img/zoomDP.jpg" class="user_pic" onclick="toggleMenu()">
 
+  <div class="sub-menu-wrap" id="subMenu">
+    <div class="sub-menu">
+      <div class="user-info">
+        <img src="img/zoomDP.jpg">
+        <h2>Raffy L. Veloria</h2>
+      </div>
+      <hr>
+
+      <a href="" class="sub-menu-link">
+      <i class="fa fa-user"></i>
+        <p>Edit Profile</p>
+        <span>></span>
+      </a>
+      <a href="" class="sub-menu-link">
+      <i class="fa fa-gear"></i>
+        <p>Setting & Privacy</p>
+        <span>></span>
+      </a>
+      <a href="" class="sub-menu-link">
+      <i class="fa-solid fa-circle-question"></i>
+        <p>Help & Support</p>
+        <span>></span>
+      </a>
+      <a href="" class="sub-menu-link">
+      <i class="fa-solid fa-right-from-bracket"></i>
+        <p>Logout</p>
+        <span>></span>
+      </a>
+    </div>
+  </div>
+
+</nav>
+<!--
 <nav>
     <input type="checkbox" id="check">
     <label for="check">
@@ -47,12 +86,14 @@
                 <span class="option-text"> <a href="">Feedback</a> </span>
             </li>
         </ul>
-    </div> </li>
+    </div>
+ </li>
+ 
         <li><a href="">Logout</a> </li>
     </ul>
 
 </nav>
-
+-->
 <!-- Digital Clock Start -->
 <div class="datetime">
     <div class="date">
@@ -72,191 +113,347 @@
 <button type="button" class="timein">Time in</button>
 <button type="button" class="timeout">Time out</button>
 
-<!--1st table-->
-<table class="userinfo">
-<tr>
-<th class="info">NAME:</th>
-<th class="info">FID:</th>
-<th class="info">PROJECTS:</th>
-<th class="info">TOTAL HOURS:</th>
-<th class="info">REQUIRED HOURS:</th>
-<th class="info"> REMAINING TIME:</th>
-</tr>
-</table>
-
-<!--Sample data 1-->
-
-
-
-
-
-
-
-
-
-<table class="mytable2" id="mytable">
-
-
- <tr class="infoget">
-
- <th class="infotable2">NO</th>
-
- <th class="infotable2">DATE</th>
-
- <th class="infotable2">IN</th>
-
- <th class="infotable2">OUT</th>
-
- <th class="infotable2">HOURS </th>
-
- <th class="infotable2">MINUTES</th>
-
- <th class="infotable2">SPECIAL EVENT</th>
-
- </tr>
-
-<!--Sample data-->
-  <tr class="infoget">
-
- <td>1</td>
-
- <td>March 27, 2023</td>
-
- <td>8:00AM</td>
-
- <td>5:00PM</td>
-
- <td>5</td>
-
- <td>0</td>
-
- <td>Typing EVent</td>
-
- </tr>
-
- <tr class="infoget">
-
- <td>2</td>
-
- <td>March 28, 2023</td>
-
- <td>8:00AM</td>
-
- <td>5:00PM</td>
-
- <td>5</td>
-
- <td>0</td>
-
- <td>WWE</td>
-
- </tr>
-
- <tr class="infoget">
-
- <td>3</td>
-
- <td>March 29, 2023</td>
-
- <td>8:00AM</td>
-
- <td>5:00PM</td>
-
- <td>5</td>
-
- <td>0</td>
-
- <td>TeamWork</td>
-
- </tr>
-
- <tr class="infoget">
-
- <td>4</td>
-
- <td>March 27, 2023</td>
-
- <td>8:00AM</td>
-
- <td>5:00PM</td>
-
- <td>5</td>
-
- <td>0</td>
-
- <td>FIS</td>
-
- </tr>
- <tr class="infoget">
-
-<td>5</td>
-
-<td>March 30, 2023</td>
-
-<td>8:00AM</td>
-
-<td>5:00PM</td>
-
-<td>5</td>
-
-<td>0</td>
-
-<td>Sleep</td>
-
-</tr>
-<tr class="infoget">
-
-<td>6</td>
-
-<td>March 31, 2023</td>
-
-<td>8:00AM</td>
-
-<td>5:00PM</td>
-
-<td>5</td>
-
-<td>0</td>
-
-<td>Eat</td>
-
-</tr>
-<tr class="infoget">
-
-<td>7</td>
-
-<td>March 27, 2023</td>
-
-<td>8:00AM</td>
-
-<td>5:00PM</td>
-
-<td>5</td>
-
-<td>0</td>
-
-<td>Getting info</td>
-
-</tr>
-<tr class="infoget">
-
-<td>8</td>
-
-<td>March 27, 2023</td>
-
-<td>8:00AM</td>
-
-<td>5:00PM</td>
-
-<td>5</td>
-
-<td>0</td>
-
-<td>Onboard</td>
-
-</tr>
-
- </table>
+<main>
+        <section class="header">
+            <div class="items-controller">
+                <h5>Show</h5>
+                <select name="" id="itemperpage">
+                <option value="04">04</option>
+                    <option value="08" selected>08</option>
+                    <option value="15">15</option>
+                </select>
+                <h5>Per Page</h5>
+            </div>
+            <div class="search">
+                <h5>Search</h5>
+                <input type="text" name="" id="search" placeholder="search">
+            </div>
+        </section>
+        <section class="field">
+            <table class="Table_User">
+                <thead>
+                     <tr class="Usern_inf">
+                      <th class="Get_info">Checkbox</th>
+                      <th class="Get_info">NO</th>
+                      <th class="Get_info">DATE</th>
+                      <th class="Get_info">IN</th>
+                      <th class="Get_info">OUT</th>
+                      <th class="Get_info">HOURS</th>
+                      <th class="Get_info">MINUTES</th>
+                      <th class="Get_info">SPECIAL EVENT</th>
+                      
+ 
+                    </tr>
+                  </thead>
+                  <!--Sample data-->
+                  <tbody>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>01</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+           
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>02</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>03</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>04</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>05</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>06</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>WWE</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>07</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>08</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>09</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>10</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>11</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>12</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>13</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>14</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                     <tr class="Usern_inf">
+                        <td><input type="checkbox"></td>
+                        <td>15</td>
+                        <td>March 28,2023</td>
+                        <td>8:00AM</td>
+                        <td>5:00PM</td>
+                        <td>8</td>
+                        <td>0</td>
+                        <td>Typing Test</td>
+                        
+                    </tr>
+                  </tbody>
+            </table>
+            <div class="bottom-field">
+                <ul class="pagination">
+                  <li class="prev"><a href="#" id="prev">&#139;</a></li>
+                    <!-- page number here -->
+                  <li class="next"><a href="#" id="next">&#155;</a></li>
+                </ul>
+            </div>
+        </section>
+    </main>
+
+    <!--Profile Intern-->
+    <script>
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu() {
+  subMenu.classList.toggle("open-menu");
+}
+
+</script>
+   
+
+
+
+<!--For Paganation -->
+<script>
+  	var tbody = document.querySelector("tbody");
+		var pageUl = document.querySelector(".pagination");
+		var itemShow = document.querySelector("#itemperpage");
+		var tr = tbody.querySelectorAll("tr");
+		var emptyBox = [];
+		var index = 1;
+		var itemPerPage = 8;
+
+		for(let i=0; i<tr.length; i++){ emptyBox.push(tr[i]);}
+
+		itemShow.onchange = giveTrPerPage;
+		function giveTrPerPage(){
+			itemPerPage = Number(this.value);
+			// console.log(itemPerPage);
+			displayPage(itemPerPage);
+			pageGenerator(itemPerPage);
+			getpagElement(itemPerPage);
+		}
+
+		function displayPage(limit){
+			tbody.innerHTML = '';
+			for(let i=0; i<limit; i++){
+				tbody.appendChild(emptyBox[i]);
+			}
+			const  pageNum = pageUl.querySelectorAll('.list');
+			pageNum.forEach(n => n.remove());
+		}
+		displayPage(itemPerPage);
+
+		function pageGenerator(getem){
+			const num_of_tr = emptyBox.length;
+			if(num_of_tr <= getem){
+				pageUl.style.display = 'none';
+			}else{
+				pageUl.style.display = 'flex';
+				const num_Of_Page = Math.ceil(num_of_tr/getem);
+				for(i=1; i<=num_Of_Page; i++){
+					const li = document.createElement('li'); li.className = 'list';
+					const a =document.createElement('a'); a.href = '#'; a.innerText = i;
+					a.setAttribute('data-page', i);
+					li.appendChild(a);
+					pageUl.insertBefore(li,pageUl.querySelector('.next'));
+				}
+			}
+		}
+		pageGenerator(itemPerPage);
+		let pageLink = pageUl.querySelectorAll("a");
+		let lastPage =  pageLink.length - 2;
+		
+		function pageRunner(page, items, lastPage, active){
+			for(button of page){
+				button.onclick = e=>{
+					const page_num = e.target.getAttribute('data-page');
+					const page_mover = e.target.getAttribute('id');
+					if(page_num != null){
+						index = page_num;
+
+					}else{
+						if(page_mover === "next"){
+							index++;
+							if(index >= lastPage){
+								index = lastPage;
+							}
+						}else{
+							index--;
+							if(index <= 1){
+								index = 1;
+							}
+						}
+					}
+					pageMaker(index, items, active);
+				}
+			}
+
+		}
+		var pageLi = pageUl.querySelectorAll('.list'); pageLi[0].classList.add("active");
+		pageRunner(pageLink, itemPerPage, lastPage, pageLi);
+
+		function getpagElement(val){
+			let pagelink = pageUl.querySelectorAll("a");
+			let lastpage =  pagelink.length - 2;
+			let pageli = pageUl.querySelectorAll('.list');
+			pageli[0].classList.add("active");
+			pageRunner(pagelink, val, lastpage, pageli);
+			
+		}
+	
+		
+		
+		function pageMaker(index, item_per_page, activePage){
+			const start = item_per_page * index;
+			const end  = start + item_per_page;
+			const current_page =  emptyBox.slice((start - item_per_page), (end-item_per_page));
+			tbody.innerHTML = "";
+			for(let j=0; j<current_page.length; j++){
+				let item = current_page[j];					
+				tbody.appendChild(item);
+			}
+			Array.from(activePage).forEach((e)=>{e.classList.remove("active");});
+     		activePage[index-1].classList.add("active");
+		}
+
+		// search content 
+		var search = document.getElementById("search");
+		search.onkeyup = e=>{
+			const text = e.target.value;
+			for(let i=0; i<tr.length; i++){
+				const matchText = tr[i].querySelectorAll("td")[0,1,2,3,4,5,6,7].innerText;
+				if(matchText.toLowerCase().indexOf(text.toLowerCase()) > -1){
+					tr[i].style.visibility = "visible";
+				}else{
+					tr[i].style.visibility= "collapse";
+				}
+			}
+		}
+</script>
 
 
 
@@ -327,3 +524,5 @@ options.forEach(option =>{
 <!-- Comment ni Sam -->
 </body>
 </html>
+
+
