2b3adf635ef79bc21689c9ec141ba2fe0753c1a5
unknown <arnold.esteban.2333@gmail.com>
2023-03-28 14:15:42 +0800

    black theme, final look of dashboard

---
+++ admin/index.js
@@ -0,0 +1,38 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+
+// show sidebar
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+//close sidebar
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+})
+
+// theme
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+// inside of List of Interns
+listOfInterns.forEach(listofintern => {
+    const tr = document.createElement('tr');
+    const trContent = `
+                        <td>${listofintern.internNo}</td>
+                        <td>${listofintern.name}</td>
+                        <td>${listofintern.project}</td>
+                        <td class="${listofintern.status === 'Inactive' ? 'danger' : listofintern.status === 'Active' ? 'warning' : 'primary'}">${listofintern.status}</td>
+                        <td class="primary">View</td>
+                        `;
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+
+})
---
+++ admin/listOfInterns.js
@@ -0,0 +1,32 @@
+const listOfInterns = [
+    {
+        internNo: 'ARE (2023-33)',
+        name: 'ARNOLD ESTEBAN',
+        project: 'Project IT',
+        status: 'Inactive'
+    },
+    {
+        internNo: 'HAT (2023-42)',
+        name: 'HAZEL TIFFANY TAYLO',
+        project: 'Project IT',
+        status: 'Active'
+    },
+    {
+        internNo: 'ZAM (2023-19)',
+        name: 'ZAIDEE MENDOZA',
+        project: 'Project IT',
+        status: 'Active'
+    },
+    {
+        internNo: 'SAP (2023-41)',
+        name: 'SAMUEL PASTOLERO',
+        project: 'Project IT',
+        status: 'Active'
+    },
+    {
+        internNo: 'ALD (2023-40)',
+        name: 'ALEXANDRA JULIAN DE GUZMAN',
+        project: 'Project IT',
+        status: 'Inactive'
+    }
+]
